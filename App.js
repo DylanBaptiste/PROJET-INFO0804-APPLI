5c3078a07103677f3128f83864cdf94ead620e7a
DylanBaptiste <dylan.baptiste@etudiant.univ-reims.fr>
2021-02-12 15:45:43 +0100

    systeme de navigation

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+function Porte(){
+	return (
+		<View style={styles.container}>
+			<Text>yo je suis la porte</Text>
+			<StatusBar style="auto" />
+		</View>
+	)
+}
+
+function Cam(){
+	return (
+		<View style={styles.container}>
+			<Text>yo je suis la camera</Text>
+			<StatusBar style="auto" />
+		</View>
+	)
+}
+
+function BDD(){
+	return (
+		<View style={styles.container}>
+			<Text>yo je suis BDD</Text>
+			<StatusBar style="auto" />
+		</View>
+	)
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+	<NavigationContainer>
+		<Tab.Navigator>
+			<Tab.Screen name="Cam" component={Cam} />
+			<Tab.Screen name="Porte" component={Porte} />
+			<Tab.Screen name="BDD" component={BDD} />
+		</Tab.Navigator>
+	</NavigationContainer>
   );
 }
 
